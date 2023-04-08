56b5bcac4a405c08efee36c2a9c375c4a02ac9dc
charmikhandor <102253770+charmikhandor@users.noreply.github.com>
2023-04-08 10:37:59 +0530

    aboot

--- src/components/About.js
+++ src/components/About.js
@@ -12,11 +12,11 @@ export default function About(props) {
             <h2 className="accordion-header">
             <button className="accordion-button collapsed" style = {{backgroundColor:props.mode==='light'?'white':'grey', 
         color:props.mode==='dark'?'white':'black'}} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                Accordion Item #1
+                About your text
             </button>
             </h2>
             <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the first item's accordion body.</div>
+            <div className="accordion-body">Textutils is a way for you to ananlyse your text quickly and efficiently</div>
             </div>
         </div>
         <div className="accordion-item" style = {{backgroundColor:props.mode==='light'?'white':'grey', 
@@ -24,11 +24,11 @@ export default function About(props) {
             <h2 className="accordion-header">
             <button className="accordion-button collapsed" style = {{backgroundColor:props.mode==='light'?'white':'grey', 
         color:props.mode==='dark'?'white':'black'}} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                Accordion Item #2
+                Free to use
             </button>
             </h2>
             <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+            <div className="accordion-body">This app is completely free to use!!</div>
             </div>
         </div>
         <div className="accordion-item" style = {{backgroundColor:props.mode==='light'?'white':'grey', 
@@ -36,11 +36,11 @@ export default function About(props) {
             <h2 className="accordion-header">
             <button className="accordion-button collapsed" style = {{backgroundColor:props.mode==='light'?'white':'grey', 
         color:props.mode==='dark'?'white':'black'}} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                Accordion Item #3
+                Browser Compactible
             </button>
             </h2>
             <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+            <div className="accordion-body">Compactible with popular browsers like chrome, firefox and edge</div>
             </div>
         </div>
         </div>
